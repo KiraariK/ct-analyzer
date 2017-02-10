162cbeef3362e5abf9923b290fdf13b9577873ac
KiraariK <kiraarik@sibmail.com>
2017-02-10 13:11:34 +0700

    Implement function for calculating max size of each region

--- CTAnalyzer/DICOMTest/SegmentsDetails.cs
+++ CTAnalyzer/DICOMTest/SegmentsDetails.cs
@@ -15,17 +15,8 @@ namespace DICOMopener
     {
         public struct Coordinates
         {
-            public int leftX;
-            public int leftY;
-
-            public int rightX;
-            public int rightY;
-
-            public int topX;
-            public int topY;
-
-            public int botomX;
-            public int botomY;
+            public int X;
+            public int Y;
         }
 
         [DllImport("CTImageSegmentation.dll", CallingConvention = CallingConvention.Cdecl)]
@@ -34,7 +25,7 @@ namespace DICOMopener
 
         private static SegmentsDetails instance = null; // Singleton
 
-        private static  int[,] regions = null;
+        private static int[,] regions = null;
         private static int _imageHeight = 0;
         private static int _imageWidth = 0;
         private static int regionsNumber = 0;
@@ -42,10 +33,11 @@ namespace DICOMopener
         // physical distances between the center of each pixel, measured in mm
         private double rowSpacing;
         private double columnSpacing;
+        private Dictionary<int, double> regionsSize = null;
 
         private Imaging.ColorFactory colorFactory = null;
 
-        private SegmentsDetails(byte[,] intencityInput, int imageHeight, int imageWidth, Array pixelSpacing, 
+        private SegmentsDetails(byte[,] intencityInput, int imageHeight, int imageWidth, Array pixelSpacing,
             int filterWidth, int intencityThreshold)
         {
             InitializeComponent();
@@ -54,6 +46,7 @@ namespace DICOMopener
             _imageWidth = imageWidth;
             rowSpacing = (double)pixelSpacing.GetValue(0, 0);
             columnSpacing = (double)pixelSpacing.GetValue(1, 0);
+            regionsSize = new Dictionary<int, double>();
 
             regions = new int[_imageHeight, _imageWidth];
             // current slice segmentation
@@ -61,6 +54,9 @@ namespace DICOMopener
 
             // generate new color factory
             colorFactory = new Imaging.ColorFactory(regionsNumber);
+
+            // calculate regions size
+            CalculateRegionsSize();
 
             FillFormControlls();
         }
@@ -72,7 +68,7 @@ namespace DICOMopener
                 instance = new SegmentsDetails(intencityInput, imageHeight, imageWidth, pixelSpacing, filterWidth, intencityThreshold);
 
             return instance;
-            
+
         }
 
         private void FillFormControlls()
@@ -108,9 +104,180 @@ namespace DICOMopener
             }
         }
 
-        private void LocateRegions()
+        private void CalculateRegionsSize()
         {
-            // TODO: implement a method to locate edge coordinates of regions
+            // from top to bottom and from left to right (min x, any y)
+            Dictionary<int, List<Coordinates>> leftCoordinates = new Dictionary<int, List<Coordinates>>();
+            for (int x = 0; x < _imageWidth; x++)
+            {
+                for (int y = 0; y < _imageHeight; y++)
+                {
+                    int regionId = regions[y, x];
+                    if (regionId == -1 || regionId == 0)
+                        continue;
+
+                    if (!leftCoordinates.ContainsKey(regionId))
+                    {
+                        List<Coordinates> newCoordinates = new List<Coordinates>();
+                        newCoordinates.Add(new Coordinates { X = x, Y = y });
+                        leftCoordinates.Add(regionId, newCoordinates);
+                    }
+                    else
+                    {
+                        for (int i = 0; i < leftCoordinates[regionId].Count; i++)
+                        {
+                            if (x == leftCoordinates[regionId].ElementAt(i).X)
+                            {
+                                leftCoordinates[regionId].Add(new Coordinates { X = x, Y = y });
+                                break;
+                            }
+                        }
+                    }
+                }
+            }
+
+            // from left to right and from top to bottom (min y, any x)
+            Dictionary<int, List<Coordinates>> topCoordinates = new Dictionary<int, List<Coordinates>>();
+            for (int y = 0; y < _imageHeight; y++)
+            {
+                for (int x = 0; x < _imageWidth; x++)
+                {
+                    int regionId = regions[y, x];
+                    if (regionId == -1 || regionId == 0)
+                        continue;
+
+                    if (!topCoordinates.ContainsKey(regionId))
+                    {
+                        List<Coordinates> newCoordinates = new List<Coordinates>();
+                        newCoordinates.Add(new Coordinates { X = x, Y = y });
+                        topCoordinates.Add(regionId, newCoordinates);
+                    }
+                    else
+                    {
+                        for (int i = 0; i < topCoordinates[regionId].Count; i++)
+                        {
+                            if (y == topCoordinates[regionId].ElementAt(i).Y)
+                            {
+                                topCoordinates[regionId].Add(new Coordinates { X = x, Y = y });
+                                break;
+                            }
+                        }
+                    }
+                }
+            }
+
+            // from top to bottom and from right to left (max x, any y)
+            Dictionary<int, List<Coordinates>> rightCoordinates = new Dictionary<int, List<Coordinates>>();
+            for (int x = _imageWidth - 1; x > 0; x--)
+            {
+                for (int y = 0; y < _imageHeight; y++)
+                {
+                    int regionId = regions[y, x];
+                    if (regionId == -1 || regionId == 0)
+                        continue;
+
+                    if (!rightCoordinates.ContainsKey(regionId))
+                    {
+                        List<Coordinates> newCoordinates = new List<Coordinates>();
+                        newCoordinates.Add(new Coordinates { X = x, Y = y });
+                        rightCoordinates.Add(regionId, newCoordinates);
+                    }
+                    else
+                    {
+                        for (int i = 0; i < rightCoordinates[regionId].Count; i++)
+                        {
+                            if (x == rightCoordinates[regionId].ElementAt(i).X)
+                            {
+                                rightCoordinates[regionId].Add(new Coordinates { X = x, Y = y });
+                                break;
+                            }
+                        }
+                    }
+                }
+            }
+
+            // from right to left and from bottom to top (max y, any x)
+            Dictionary<int, List<Coordinates>> bottomCoordinates = new Dictionary<int, List<Coordinates>>();
+            for (int y = _imageHeight - 1; y > 0; y--)
+            {
+                for (int x = _imageWidth - 1; x > 0; x--)
+                {
+                    int regionId = regions[y, x];
+                    if (regionId == -1 || regionId == 0)
+                        continue;
+
+                    if (!bottomCoordinates.ContainsKey(regionId))
+                    {
+                        List<Coordinates> newCoordinates = new List<Coordinates>();
+                        newCoordinates.Add(new Coordinates { X = x, Y = y });
+                        bottomCoordinates.Add(regionId, newCoordinates);
+                    }
+                    else
+                    {
+                        for (int i = 0; i < bottomCoordinates[regionId].Count; i++)
+                        {
+                            if (y == bottomCoordinates[regionId].ElementAt(i).Y)
+                            {
+                                bottomCoordinates[regionId].Add(new Coordinates { X = x, Y = y });
+                                break;
+                            }
+                        }
+                    }
+                }
+            }
+
+            // calculate horizontal oriented sizes
+            Dictionary<int, double> horizontalSizes = new Dictionary<int, double>();
+            foreach (KeyValuePair<int, List<Coordinates>> it in leftCoordinates)
+            {
+                if (rightCoordinates.ContainsKey(it.Key))
+                {
+                    List<double> regionHorizontalSizes = new List<double>();
+                    for (int i = 0; i < it.Value.Count; i++)
+                    {
+                        for (int j = 0; j < rightCoordinates[it.Key].Count; j++)
+                        {
+                            regionHorizontalSizes.Add
+                            (
+                                Math.Sqrt(
+                                    Math.Pow(((it.Value.ElementAt(i).X - rightCoordinates[it.Key].ElementAt(j).X) * rowSpacing), 2) +
+                                    Math.Pow(((it.Value.ElementAt(i).Y - rightCoordinates[it.Key].ElementAt(j).Y) * columnSpacing), 2)
+                                )
+                            );
+                        }
+                    }
+                    horizontalSizes.Add(it.Key, regionHorizontalSizes.Max());
+                }
+            }
+
+            // calculate vertical oriented sizes
+            Dictionary<int, double> verticalSizes = new Dictionary<int, double>();
+            foreach (KeyValuePair<int, List<Coordinates>> it in topCoordinates)
+            {
+                if (bottomCoordinates.ContainsKey(it.Key))
+                {
+                    List<double> regionVerticalSizes = new List<double>();
+                    for (int i = 0; i < it.Value.Count; i++)
+                    {
+                        for (int j = 0; j < bottomCoordinates[it.Key].Count; j++)
+                        {
+                            regionVerticalSizes.Add
+                            (
+                                Math.Sqrt(
+                                    Math.Pow(((it.Value.ElementAt(i).X - bottomCoordinates[it.Key].ElementAt(j).X) * rowSpacing), 2) +
+                                    Math.Pow(((it.Value.ElementAt(i).Y - bottomCoordinates[it.Key].ElementAt(j).Y) * columnSpacing), 2)
+                                )
+                            );
+                        }
+                    }
+                    verticalSizes.Add(it.Key, regionVerticalSizes.Max());
+                }
+            }
+
+            // select max size between horizontal and vertical sizes
+            foreach(KeyValuePair<int, double> it in horizontalSizes)
+                if (verticalSizes.ContainsKey(it.Key))
+                    regionsSize.Add(it.Key, Math.Max(it.Value, verticalSizes[it.Key]));
         }
 
         /// <summary>
@@ -136,6 +303,7 @@ namespace DICOMopener
         {
             regions = null;
             colorFactory = null;
+            regionsSize = null;
             instance = null;
 
             ClearGarbage();
